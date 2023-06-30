fba82a6bd5080a95ab2756784091762a3c6eeed2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-30 23:49:34 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -16,17 +16,12 @@ for idx, entrie in enumerate(rss_feed['entries']):
 
 preREADME = """
 #### 책을 좋아하는 개발자📚
-"지식의 확장과 성장을 즐기며 이를 활용해 문제 해결과 성취감을 찾는 개발자입니다."
+"기술에 대한 호기심을 가지고 꾸준히 성장하는 개발자"
 
 ###### Skills
 <img src="https://img.shields.io/badge/java-c74634?style=flat-square&logo=oracle&logoColor=white"> <img src="https://img.shields.io/badge/spring-6DB33F?style=flat-square&logo=spring&logoColor=white"> <img src="https://img.shields.io/badge/mysql-4479A1?style=flat-square&logo=mysql&logoColor=white">
 
 <a href="https://github.com/syeon2"><img src="https://img.shields.io/badge/github-181717?style=flat-square&logo=github&logoColor=white"></a> <a href="https://fascinated-beechnut-581.notion.site/Daebi-s-Devlog-f2c82ac119d44e9eb5a46988b0882c13"><img src="https://img.shields.io/badge/notion-181717?style=flat-square&logo=Notion&logoColor=white" /></a> <img src="https://img.shields.io/badge/slack-6441A5?style=flat-square&logo=Slack&logoColor=white" />
-
-###### Blog
-<a href="https://syeon2.github.io/" target="_blank">
-  <img src="https://img.shields.io/badge/Devlog-000000?style=flat-square&logo=Bitdefender&logoColor=white"/></a>
-</a>
 
 ------
 #### 💁🏻‍♂️ Latest Blog Post
