a0e1af7d57706fbe54800a0444694fa9e44320f4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-06-15 15:07:09 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -16,7 +16,7 @@ for idx, entrie in enumerate(rss_feed['entries']):
 
 preREADME = """
 ### 책을 좋아하는 개발자📚
-"기술에 대한 호기심을 가지고 꾸준히 성장합니다."
+"기술에 대한 호기심을 가지고 꾸준히 성장합니다. "
 
 ###### Skills
 <img src="https://img.shields.io/badge/java-c74634?style=flat-square&logo=oracle&logoColor=white"> <img src="https://img.shields.io/badge/spring-6DB33F?style=flat-square&logo=spring&logoColor=white"> <img src="https://img.shields.io/badge/mysql-4479A1?style=flat-square&logo=mysql&logoColor=white"> <img src="https://img.shields.io/badge/redis-DC382D?style=flat-square&logo=redis&logoColor=white">
